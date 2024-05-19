1552ea4239412f05b412d52905a7d5dae8089ef8
Xianyi Liang <liangxianyi2011@gmail.com>
2024-05-19 03:25:57 -0400

    add code

--- src/app/motion/basics/page.tsx
+++ src/app/motion/basics/page.tsx
@@ -1,6 +1,67 @@
 import React from "react";
 import ShowHide from "./ShowHide";
 import DragGesture from "./DragGesture";
+import { Code } from "bright";
+const dragCode = `
+export default function DragGesture() {
+  const constraintRef = useRef(null);
+  return (
+    <div
+      ref={constraintRef}
+      className="relative h-96 w-96 border-2 border-dashed border-sky-600"
+    >
+      <p className=" absolute top-4 left-1/2 -translate-x-1/2 text-sky-600">
+        Drag Anya
+      </p>
+      <MotionImage
+        drag
+        whileDrag={{ rotate: "180deg" }}
+        dragConstraints={constraintRef}
+        className=" w-72 h-72"
+        src={anyaShocked}
+        width={720}
+        height={700}
+        alt="Anya Crying"
+      />
+    </div>
+  );
+}
+`;
+const toggleCode = `
+export default function ShowHide() {
+  const [show, setShow] = useState(true);
+  const id = useId();
+  const imgid = '\${id}-img';
+
+  return (
+    <div className=" flex flex-col items-center">
+      <AnimatePresence>
+        {show && (
+          <motion.div
+            key={imgid}
+            className=" w-96 h-96"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 1, type: "spring" }}
+          >
+            <Image
+              key={imgid}
+              src={anyaCrying}
+              width={720}
+              height={700}
+              alt="Anya Crying"
+            />
+          </motion.div>
+        )}
+        <Button layout className="" onClick={() => setShow((cur) => !cur)}>
+          {show ? "Hide" : "Show"}
+        </Button>
+      </AnimatePresence>
+    </div>
+  );
+}
+`;
 
 export default function Page() {
   return (
@@ -13,6 +74,8 @@ export default function Page() {
         <ShowHide />
         <DragGesture />
       </div>
+      <Code lang="js">{toggleCode}</Code>
+      <Code lang="js">{dragCode}</Code>
     </div>
   );
 }
