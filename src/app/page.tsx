182a68485481683b497fc0778d06cc92476cf161
Xianyi Liang <liangxianyi2011@gmail.com>
2024-05-19 21:38:54 -0400

    update template file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,26 +2,22 @@ import { Code } from "bright";
 
 const code = `
 // template.tsx
-export default function Transition({ children }: Props) {
-  const pathname = usePathname();
+export default function Template({ children }: Props) {
   return (
-    <AnimatePresence mode="wait" key={pathname}>
-      <motion.div
-        key={pathname}
-        initial={{
-          opacity: 0,
-          clipPath: "polygon(50% 0, 50% 0, 50% 100%, 50% 100%)",
-        }}
-        animate={{
-          rotate: "0deg",
-          opacity: 1,
-          clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)",
-        }}
-        transition={{ ease: "easeInOut", duration: 0.8 }}
-      >
-        {children}
-      </motion.div>
-    </AnimatePresence>
+    <motion.div
+      initial={{
+        opacity: 0,
+        clipPath: "polygon(50% 0, 50% 0, 50% 100%, 50% 100%)",
+      }}
+      animate={{
+        rotate: "0deg",
+        opacity: 1,
+        clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)",
+      }}
+      transition={{ ease: "easeInOut", duration: 0.8 }}
+    >
+      {children}
+    </motion.div>
   );
 }
 `;
--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,32 +1,27 @@
 "use client";
 
 import React from "react";
-import { motion, AnimatePresence } from "framer-motion";
-import { usePathname } from "next/navigation";
+import { motion } from "framer-motion";
 
 interface Props {
   children: React.ReactNode;
 }
 
-export default function Transition({ children }: Props) {
-  const pathname = usePathname();
+export default function Template({ children }: Props) {
   return (
-    <AnimatePresence mode="wait" key={pathname}>
-      <motion.div
-        key={pathname}
-        initial={{
-          opacity: 0,
-          clipPath: "polygon(50% 0, 50% 0, 50% 100%, 50% 100%)",
-        }}
-        animate={{
-          rotate: "0deg",
-          opacity: 1,
-          clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)",
-        }}
-        transition={{ ease: "easeInOut", duration: 0.8 }}
-      >
-        {children}
-      </motion.div>
-    </AnimatePresence>
+    <motion.div
+      initial={{
+        opacity: 0,
+        clipPath: "polygon(50% 0, 50% 0, 50% 100%, 50% 100%)",
+      }}
+      animate={{
+        rotate: "0deg",
+        opacity: 1,
+        clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)",
+      }}
+      transition={{ ease: "easeInOut", duration: 0.8 }}
+    >
+      {children}
+    </motion.div>
   );
 }
