cdbcbfa32b0e4fd8be4df98880385413b6ab0e09
Xianyi Liang <liangxianyi2011@gmail.com>
2024-05-20 16:50:53 -0400

    Disable btn when click

--- src/app/motion/timeline-animation/UseAnimate.tsx
+++ src/app/motion/timeline-animation/UseAnimate.tsx
@@ -1,10 +1,12 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { useAnimate } from "framer-motion";
 
 export default function UseAnimate() {
   const [scope, animate] = useAnimate();
+  const [btnActive, setBtnActive] = useState(true);
   async function handleClick() {
+    setBtnActive(false);
     animate("button", { opacity: 0, scale: 0 }, { duration: 2 });
     await animate(
       "#box",
@@ -27,11 +29,13 @@ export default function UseAnimate() {
       { duration: 0.7, type: "spring" }
     );
     animate("button", { opacity: 1, scale: 1 }, { duration: 1 });
+    setBtnActive(true);
   }
   return (
     <div ref={scope} className=" grid h-screen place-content-center">
       <div id="box" className=" w-72 h-72 rounded-sm bg-purple-600"></div>
       <button
+        disabled={!btnActive}
         onClick={handleClick}
         className="px-6 py-4 rounded-md bg-gray-800 text-white mt-6"
       >
