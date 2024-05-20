afb8c1ddbe914815bdeb7a05b3ea4b8e42587514
Xianyi Liang <liangxianyi2011@gmail.com>
2024-05-20 16:51:34 -0400

    Update code

--- src/app/motion/timeline-animation/page.tsx
+++ src/app/motion/timeline-animation/page.tsx
@@ -4,7 +4,9 @@ import UseAnimate from "./UseAnimate";
 const code = `
 export default function UseAnimate() {
   const [scope, animate] = useAnimate();
+  const [btnActive, setBtnActive] = useState(true);
   async function handleClick() {
+    setBtnActive(false);
     animate("button", { opacity: 0, scale: 0 }, { duration: 2 });
     await animate(
       "#box",
@@ -27,11 +29,13 @@ export default function UseAnimate() {
       { duration: 0.7, type: "spring" }
     );
     animate("button", { opacity: 1, scale: 1 }, { duration: 1 });
+    setBtnActive(true);
   }
   return (
     <div ref={scope} className=" grid h-screen place-content-center">
       <div id="box" className=" w-72 h-72 rounded-sm bg-purple-600"></div>
       <button
+        disabled={!btnActive}
         onClick={handleClick}
         className="px-6 py-4 rounded-md bg-gray-800 text-white mt-6"
       >
